8a0808bdce5e3e3488de4ab069bd0fcc709cecce
Sam Pratt <samuel.pratt@gmail.com>
2021-01-01 14:56:07 +0000

    Added Async bit from "Pt4: Add Services".

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +10,19 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   hero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
